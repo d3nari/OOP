2e23200dce9868cd5cc307f170d4c725667b05b5
d3nari <dungerway@gmail.com>
2022-02-14 19:06:11 +0300

    upd

---
+++ lab1/task1/copytext/copytext/copytext.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[]) 
+{
+	if (argc != 3) 
+	{
+		std::cout << "Invalid arguments count \n";
+		std::cout << "Usage: CopyFile.exe <input file name> <output file name> \n";
+		return 1;
+	}
+
+	std::ifstream input;
+
+	input.open(argv[1]);
+	if (!input.is_open()) 
+	{
+		std::cout << "Fail to open '" << argv[1] << "' for reading";
+		return 1;
+	}
+
+	std::ofstream output;
+	output.open(argv[2]);
+	if (!output.is_open()) 
+	{
+		std::cout << "Fail to open '" << argv[2] << "' for writing";
+		return 1;
+	}
+
+
+
+	char ch;
+	while (input.get(ch))
+	{
+		output.put(ch);
+	}
+
+    /*std::cout << argc << "\n";
+	for (int i = 0; i < argc; ++i)
+	{
+		std::cout << argv[i] << "\n";
+	}*/
+    return 0;
+}
+
